4dee984d8f25a0072be9596d9b2caf360154c1bf
ao-connor <adrian@adrianoc.com>
2016-03-23 17:11:37 +0000

    major chords working

--- lib/DDG/Goodie/Pianochord.pm
+++ lib/DDG/Goodie/Pianochord.pm
@@ -21,23 +21,39 @@ my @minor_scale_intervals = (2,1,2,2,1,2,2);
 my @harmonic_minor_scale_intervals = (2,1,2,2,1,3,1);
 my @ascending_melodic_minor_scale_intervals = (2,1,2,2,1,3,2);
 
+my @triad_intervals = (0,2,4);
+
 my @note_names = ('A','A#','B','C','C#','D','D#','E','F','F#','G','G#');
 
 # Handle statement
 handle remainder => sub {
+    our $note_name;
 
     my $remainder = $_;
-
-    for( $curNote = 0; $curNote < length @note_names ; $curNote = $curNote + 1 ){
-        if ( @note_names[$curNote] eq uc $remainder ) {
+    
+    
+    for my $curNote (0 .. scalar(@note_names)-1 )  {
+        if ( $note_names[$curNote] eq uc $remainder ) {
+            $note_name = $note_names[$curNote];
+            my @chordNotes = ();
+            my $intervalTotal = 0;
+            for my  $curInterval (0 .. scalar(@major_scale_intervals)-1 )  {
+                for my $curChordNote (0 .. scalar(@triad_intervals)-1 )  {
+                    if ( $curInterval == $triad_intervals[$curChordNote] ) {
+                        my $index = (12 + $curNote + $intervalTotal ) % 12;
+                        push @chordNotes, $note_names[$index];
+                    }
+                }
+                $intervalTotal = $intervalTotal + $major_scale_intervals[$curInterval];
+            }
             return
-                heading => "Piano @note_names[$curNote]maj Chord",
-                answer => "C E G",
-                html => "<span>notes C E G</span>";
+                heading => "Piano $note_name maj Chord",
+                answer => join(" ", @chordNotes),
+                html => "<span>Notes " . join(" ", @chordNotes) . "</span>";
         }
     }
 
-    return
+    return;
 };
 
 1;
